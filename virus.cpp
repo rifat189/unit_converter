8080f0ae50c7985b7ad6439af597c3ab57379166
rifat189 <saidulrifat189@gmail.com>
2025-10-19 14:45:59 +0600

    new

---
+++ virus.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Why did the computer go to the doctor?" << endl;
+    cout << "Because it had a virus!" << endl;
+    return 0;
+}
